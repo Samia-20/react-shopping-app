52bf6ef5bbc661037b1d12cfd0e546dbe1294ca5
raomr <mrinalrao1810@gmail.com>
2023-12-27 16:31:24 +0530

    stepper skeleton

--- src/components/OrderPage.jsx
+++ src/components/OrderPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useLocation } from 'react-router';
+import { useState } from 'react';
 import Box from '@mui/material/Box';
 import Stepper from '@mui/material/Stepper';
 import Step from '@mui/material/Step';
@@ -14,92 +15,82 @@ export default function OrderPage() {
    const product = location.state.product;
    const quantity = location.state.quantity;
 
-     const [activeStep, setActiveStep] = React.useState(0);
-     const [skipped, setSkipped] = React.useState(new Set());
-   
-     
-   
-     const isStepSkipped = (step) => {
-       return skipped.has(step);
-     };
-   
-     const handleNext = () => {
-       let newSkipped = skipped;
-       if (isStepSkipped(activeStep)) {
-         newSkipped = new Set(newSkipped.values());
-         newSkipped.delete(activeStep);
-       }
-   
-       setActiveStep((prevActiveStep) => prevActiveStep + 1);
-       setSkipped(newSkipped);
-     };
-   
-     const handleBack = () => {
-       setActiveStep((prevActiveStep) => prevActiveStep - 1);
-     };
-   
-    
-   
-     const handleReset = () => {
-       setActiveStep(0);
-     };
-   
-     return (
-       <Box sx={{ width: '100%' }}>
-         <Stepper activeStep={activeStep}>
-           {steps.map((label, index) => {
-             const stepProps = {};
-             const labelProps = {};
-             return (
-               <Step key={label} {...stepProps}>
-                 <StepLabel {...labelProps}>{label}</StepLabel>
-               </Step>
-             );
-           })}
-         </Stepper>
-         {activeStep === steps.length ? (
-           <React.Fragment>
-             <Typography sx={{ mt: 2, mb: 1 }}>
-               All steps completed - you're finished
-             </Typography>
-             <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-               <Box sx={{ flex: '1 1 auto' }} />
-               <Button onClick={handleReset}>Reset</Button>
-             </Box>
-           </React.Fragment>
-         ) : (
-           <React.Fragment>
-            {(activeStep === 0)?(
-                
-                <div className='items'>
-                    <p>OrderPage quantity: {quantity}</p> 
-                    <p>Product Price: {product.price}</p>
-                    <p>Total Price: {product.price * quantity}</p>
-                 </div>
-                 
-            ): (
-                <Typography sx={{ mt: 2, mb: 1 }}>
-               Test String
-             </Typography>
-            ) }
-             <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-               <Button
-                 color="inherit"
-                 disabled={activeStep === 0}
-                 onClick={handleBack}
-                 sx={{ mr: 1 }}
-               >
-                 Back
-               </Button>
-               <Box sx={{ flex: '1 1 auto' }} />
-   
-               <Button onClick={handleNext}>
-                 {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
-               </Button>
-             </Box>
-           </React.Fragment>
-         )}
-       </Box>
-     );
+   const [activeStep, setActiveStep] = useState(0);
+   const [items, setItems] = useState([]);
+   const [address, setAddress] = useState('');
+   // Add any other necessary state for each step
+ 
+   const handleNext = () => {
+     setActiveStep((prevActiveStep) => prevActiveStep + 1);
+   };
+ 
+   const handleBack = () => {
+     setActiveStep((prevActiveStep) => prevActiveStep - 1);
+   };
+ 
+   const getStepContent = (stepIndex) => {
+     switch (stepIndex) {
+       case 0:
+         return <ItemsStep items={items} setItems={setItems} />;
+       case 1:
+         return <AddressStep address={address} setAddress={setAddress} />;
+       case 2:
+         return <ConfirmOrderStep items={items} address={address} />;
+       default:
+         return 'Unknown stepIndex';
+     }
+   };
+   const ItemsStep = ({ items, setItems }) => {
+    return (
+      <div>
+        <h2>Items</h2>
+        {/* Add your component content for the Items step */}
+      </div>
+    );
+  };
+
+  const AddressStep = ({ address, setAddress }) => {
+    return (
+      <div>
+        <h2>Enter Address</h2>
+        {/* Add your component content for the Address step */}
+      </div>
+    );
+  };
+
+  const ConfirmOrderStep = ({ items, address }) => {
+    return (
+      <div>
+        <h2>Confirm Order</h2>
+        {/* Add your component content for the Confirm Order step */}
+      </div>
+    );
+  };
+
+   return (
+     <div>
+       <Stepper activeStep={activeStep} alternativeLabel>
+         {steps.map((label) => (
+           <Step key={label}>
+             <StepLabel>{label}</StepLabel>
+           </Step>
+         ))}
+       </Stepper>
+       <div>
+         <Typography component="div" style={{ margin: '20px 0' }}>
+           {getStepContent(activeStep)}
+         </Typography>
+         <div>
+           <Button disabled={activeStep === 0} onClick={handleBack}>
+             Back
+           </Button>
+           <Button variant="contained" color="primary" onClick={handleNext}>
+             {activeStep === steps.length - 1 ? 'Place Order' : 'Next'}
+           </Button>
+         </div>
+       </div>
+     </div>
+   );
+
   
 }
